14a6e4cebd7c89687360b587a1bbb785eb7464b4
ryanmanchester <ryanm17@gmail.com>
2019-06-06 18:45:02 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,17 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let i = 0;
-function init() {
-  document.body.addEventListener('keydown', function(e) {
-    let key = parseInt(e.which);
-    if (key === code[i]) {
-      i++;
-      if (i === code.length) {
-        alert('Konami code entered!')
-      }
 
+function init() {
+  let i = 0;
+  document.body.addEventListener('keydown', function(e) {
+    let key = parseInt(e.which || e.detail);
+    if (key === code[i]) {
+        i++;
+        if (i === code.length) {
+            alert('Konami code entered!');
+        }
     } else {
       i = 0;
     }
-  })
+});
 }
 init();
