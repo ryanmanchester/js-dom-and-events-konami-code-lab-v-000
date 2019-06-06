b971052b07a7731e7fca47d46b9b38f9616d688c
ryanmanchester <ryanm17@gmail.com>
2019-06-06 18:36:02 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let i = 0;
 function init() {
- //set up a for loop
- //check the keydown value against the code if...else
+  document.body.addEventListener('keydown', function(e) {
+    let key = parseInt(e.which);
+    if (key === code[i]) {
+      i++;
+      if (i === code.length) {
+        alert('Konami code entered!')
+      }
 
+    } else {
+      i = 0;
+    }
+  })
 }
+init();
